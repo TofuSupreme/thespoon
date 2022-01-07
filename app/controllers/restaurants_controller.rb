ad231140a766e60216e5637d3a75a167d4739244
tofusupreme <yunishbacon@gmail.com>
2022-01-07 18:00:00 +0900

    Added updated create and edit methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,17 +9,28 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+  # This is a safety feature or 'strong params'. This enables that whatever is being passed from the restaurant form does not get mutated by a hacker. ONLY the required values gets passed. NOTHING else.
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
   end
 end
