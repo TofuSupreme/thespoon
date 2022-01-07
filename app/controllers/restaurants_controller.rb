a12cada38031b6d93c536e3b16a13120717b253b
tofusupreme <yunishbacon@gmail.com>
2022-01-08 08:59:55 +0900

    Updated routes, methods, and refactored

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,12 @@
 class RestaurantsController < ApplicationController
+  # for the refatoring method
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
   def index
     # see all restaurants
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -19,18 +20,27 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
   # This is a safety feature or 'strong params'. This enables that whatever is being passed from the restaurant form does not get mutated by a hacker. ONLY the required values gets passed. NOTHING else.
+
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
+  # for refactoring
+  def find_restaurant
+  @restaurant = Restaurant.find(params[:id])
   end
 end
